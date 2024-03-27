33931818808a80e91700829cbbd1f3855e7e071a
breach-forum <157008483+breach-forum@users.noreply.github.com>
2024-03-27 12:05:11 -0300

    Create Soma.js
    Soma de dois números decimais

---
+++ Soma.js
@@ -0,0 +1,11 @@
+// Pedir o primeiro número decimal
+var numero1 = parseFloat(prompt("Digite o primeiro número decimal: "));
+
+// Pedir o segundo número decimal
+var numero2 = parseFloat(prompt("Digite o segundo número decimal: "));
+
+// Somar os dois números
+var soma = numero1 + numero2;
+
+// Imprimir o resultado
+console.log("A soma dos dois números é: " + soma);
